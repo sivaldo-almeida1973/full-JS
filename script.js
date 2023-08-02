fafa57d2609169916f88acf704a248624e518404
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-08-02 10:29:25 -0300

    curso de JS

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 alert('oi,seja bem vindo ao curso!!!')
-document.getElementById("nome").value = "oi!"
+
+
+/* selcionar um elemento no DOM atualizar o valor desse elemento com uma string */
+document.getElementById("nome").value = "Sivaldo Vieira de Almeida!"
+
+//comentários de uma linha em js
+
+/*comentario de multiplas linhas encapsuladas em um mesmo comentário */
